4db6fb3e361eb21f79975ebc4f47d3fc2057d859
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-05-03 10:44:29 +0400

    initial commite

---
+++ public/components/registration/Registration.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {reduxForm } from 'redux-form';
+import style from './Registration.module.css';
+import {setUserData} from '../../dal//connectApi';
+import RegisterForm from './registrationForm/RegistrationForm';
+
+const ReduxForm = reduxForm({form: "registration"})(RegisterForm);
+
+const Registration = () => {
+    
+    const register = (value) => {
+        setData(value);
+    }
+
+    return (
+        <div className={style.container}>
+            <ReduxForm onSubmit={register}/>
+        </div>
+    )
+}
+
+function setData(value){
+    //create data
+    const user = {
+        name: value.name,
+        card: value.card,
+        address: value.address,
+        phone: value.phone,
+        drugName: value.drugName,
+        buyMedicine: value.buyMedicine,
+        doctorVat: value.doctorVAT,
+        doctorPhone: value.doctorPhone,
+        guardianVAT: value.guardianVAT,
+        guardianPhones: value.guardianPhones,
+    }
+    
+    //add data firebase
+    setUserData(user);
+}
+
+export default Registration;
